55035211b816407bf3078001b42315bb3f319718
Zahit Kaya <Zahit.Kaya@analog.com>
2023-12-19 12:22:10 +0100

    Removed extra lines and added new line to the end. Copyright date set to 2023.

--- projects/adi_vna_8ch/zcu102/system_bd.tcl
+++ projects/adi_vna_8ch/zcu102/system_bd.tcl
@@ -1,9 +1,6 @@
 ###############################################################################
-
-## Copyright (C) 2023-2023 Analog Devices, Inc. All rights reserved.
-
+## Copyright (C) 2023 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
-
 ###############################################################################
 
 set dac_fifo_address_width 16
@@ -18,4 +15,3 @@ ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
 ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "[pwd]/mem_init_sys.txt"
 ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
 sysid_gen_sys_init_file
-
